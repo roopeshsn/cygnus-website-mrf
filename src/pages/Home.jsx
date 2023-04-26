aef1513a179b03e1f0383f5ad3d7701708ea81a3
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2023-04-26 18:32:09 +0530

    Added Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import {
+  Container,
+  Typography,
+  Button,
+  Box,
+  Link as MuiLink,
+} from '@mui/material'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container>
+      <Box sx={{ mt: '2rem' }}>
+        <Typography variant="h4" component="h1">
+          Cygnus 2023
+        </Typography>
+        <Typography sx={{ mt: '1rem' }} variant="h6">
+          We're still working on this!
+        </Typography>
+        <Link to="/login">
+          <MuiLink
+            sx={{ display: 'block', fontSize: '1.2rem' }}
+            variant="outlined"
+            component="button"
+          >
+            Login
+          </MuiLink>
+        </Link>
+      </Box>
+    </Container>
   )
 }
