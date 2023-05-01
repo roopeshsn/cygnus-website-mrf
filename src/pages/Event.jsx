b620788652bba6f30c49a738afba8d3f617fdd6d
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2023-05-01 22:32:41 +0530

    [Fix] heading in individual event page

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -20,10 +20,10 @@ export default function Event() {
       <section className="bg-white mb-8">
         <div className="grid max-w-screen-xl px-4 py-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
               {eventData.title}
             </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
               {eventData.description || 'Nothing'}
             </p>
             <Link
